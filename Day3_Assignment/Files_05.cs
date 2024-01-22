e046af05615772247b3c2690e630727d971f1780
shaikmoulana <57175885+shaikmoulana@users.noreply.github.com>
2024-01-22 06:04:51 -0800

    Add files via upload

---
+++ Day3_Assignment/Files_05.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day3_Assignment
+{
+    class Files_05
+    {
+        public void write()
+        {
+            string data = "hello world";
+            File.WriteAllText("C:\\Users\\mshaik5\\source\\repos\\Day3_Assignment\\file1.txt", data);
+        }
+
+        public void read()
+        {
+            string read = File.ReadAllText("C:\\Users\\mshaik5\\source\\repos\\Day3_Assignment\\file1.txt");
+            Console.WriteLine(read);
+            
+        }
+
+        public void delete()
+        {
+
+        }
+
+    }
+}
